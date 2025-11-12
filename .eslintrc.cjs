bf2eb9805700d481c712969e859860aec56db70b
artsemrogovenko <taftoos2@gmail.com>
2025-11-12 15:56:28 +0300

    init: start websocket task

---
+++ .eslintrc.cjs
@@ -0,0 +1,19 @@
+module.exports= {
+  env: {
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
+  ignorePatterns: ['.eslintrc.js', '*.config.ts', 'dist/**/*'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+};
